565be57f7e67dfc3a4ac3fdf2b37e9ad9b47a98a
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-29 19:48:38 -0400

    set my memeber state and values state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import Form from './Components/Form';
 
 function App() {
+
+  const [members, setMembers] = useState([]);
+  const [values, setValues] = useState({name: '',email: '',role: ''});
+
   return (
     <div className="App">
       <h1>Team Builder App</h1>
