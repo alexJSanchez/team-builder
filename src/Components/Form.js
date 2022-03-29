dcdec7fe2afba4856fec7f4525db73d66fd81229
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-29 18:26:59 -0400

    basic name input

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Form = (props) => {
     return(
         <form>
-            
+            <label>Name
+            <input 
+                placeholder='Your Name Here'
+                value={values.name}
+                name='name'
+                onChange={handleChange}
+            />
+            </label>
         </form>
     )
 }
+
+export default Form;
