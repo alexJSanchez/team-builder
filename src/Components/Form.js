bf2d8bae6bd7c086e12c756ea5722f0cc8f63f7d
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-29 18:50:36 -0400

    make a function to handle value changes

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Form = (props) => {
+
+    const handleChange = Event => {
+        const { name, value } = Event.target;
+
+        props.change(name,value);
+    }
+
     return(
         <form>
             <label>Name
@@ -11,6 +18,24 @@ const Form = (props) => {
                 onChange={handleChange}
             />
             </label>
+            <label>Email
+            <input 
+                placeholder='Your Email Here'
+                value={values.email}
+                name='email'
+                onChange={handleChange}
+            />
+            </label>
+            <label>Role
+            <input 
+                placeholder='What is Your Role'
+                value={values.role}
+                name='role'
+                onChange={handleChange}
+            />
+            </label>
+            <input type='submit' value='Create Your Profile'>
+            </input>
         </form>
     )
 }
